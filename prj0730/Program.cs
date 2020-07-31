922040e9b8389a7111c2e4a6cacb304d492ed9af
Your Name <you@example.com>
2020-07-31 11:33:02 +0900

    1

---
+++ prj0730/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace prj0730
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello hoho!");
+        }
+    }
+}
